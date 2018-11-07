de60a6ab5046f69b2ac53f80f8bed2127da108d2
rdm-dev03 <39398838+rdm-dev03@users.noreply.github.com>
2018-11-07 10:07:01 +0900

    delete dependencies
    fixed unit test error

--- addons/ftp/migrations/0001_initial.py
+++ addons/ftp/migrations/0001_initial.py
@@ -12,10 +12,6 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        ('osf', '0134_abstractnode_custom_citation'),
-    ]
-
     operations = [
         migrations.CreateModel(
             name='NodeSettings',
